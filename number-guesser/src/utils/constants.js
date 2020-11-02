b537cc6da59c04315692c9cf2403f1ab051efa2c
thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2020-11-02 11:16:10 -0300

    edit constant

--- number-guesser/src/utils/constants.js
+++ number-guesser/src/utils/constants.js
@@ -1,7 +1,7 @@
 export const howTitle = 'Instructions'
 export const howText = `For play this game you should think on a prime number between 1 and 8000 and the computer must guess which number it is. You should press less if you number is smaller than the one guessed by the computer, or bigger if it is bigger, and same if it is the same. Plus that, while the game go on, you also can give some tips for the computer, the options are:
 1 - Indicate the sum of the digits.
-2 - indicate the rest of the division by seven.
+2 - Indicate the rest of the division by seven.
 3 - Indicate the product of the digits that make up the number, not counting leading zeros.
 You just can give each tip once.
 The faster you get the computer to guess your number, the better`
